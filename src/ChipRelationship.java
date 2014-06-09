a296cf6ea64bd5fb149aec0c613fa038207b7985
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2014-06-09 19:56:26 +0200

    modify order enum to be able to use Enum.values()

--- src/ChipRelationship.java
+++ src/ChipRelationship.java
@@ -2,14 +2,16 @@
  * Created by jrm2k6 on 6/9/14.
  */
 public enum ChipRelationship {
-    LEFT_NEIGHBOR,
-    RIGHT_NEIGHBOR,
+    // Do not modify the order if you want to be able to rely
+    // on ChipRelationship.values()[index] to access elements
     TOP_NEIGHBOR,
-    BOTTOM_NEIGHBOR,
-    TOP_LEFT_NEIGHBOR,
-    BOTTOM_RIGHT_NEIGHBOR,
     TOP_RIGHT_NEIGHBOR,
-    BOTTOM_LEFT_NEIGHBOR;
+    RIGHT_NEIGHBOR,
+    BOTTOM_RIGHT_NEIGHBOR,
+    BOTTOM_NEIGHBOR,
+    BOTTOM_LEFT_NEIGHBOR,
+    LEFT_NEIGHBOR,
+    TOP_LEFT_NEIGHBOR;
 
     private ChipRelationship opposite;
 
